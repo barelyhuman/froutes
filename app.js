fa90637ef8209887297b3bdf16121a24f0c4d70e
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-06-12 19:12:46 +0530

    ora addition, processer change, additional utils
    -  Added Ora for cli spinners
    - moved file tree processor out
    - custom director for .route to store files and state

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const microServer = require('./lib/micro-server');
+const setupRoutes = require('./lib/setup-routes');
 const http = require('http');
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-http.createServer(
-    (req,res)=>{
-        microServer(req,res);
-    }  
-).listen(PORT,()=>{
-    console.log("> Listening on "+PORT);
+setupRoutes();
+
+http.createServer((req, res) => {
+    microServer(req, res);
+}).listen(PORT, () => {
+    console.log('> Listening on ' + PORT);
 });
-
-
-
-
