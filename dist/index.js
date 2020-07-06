a8dde05368d42c935fdc3d803052aaec01f7dca9
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-07-06 14:58:29 +0530

    updated perf stats

--- dist/index.js
+++ dist/index.js
@@ -1376,13 +1376,21 @@ const querystring = __webpack_require__(191)
 
 module.exports = (urlstring) => {
     const _url = url.parse(urlstring)
-    const paths = _url.pathname.split('/').filter((item) => item)
+    const paths = _url.pathname.split('/')
+    let _paths = []
+
+    for (let i = 0; i < paths.length; i += 1) {
+        if (paths[i]) {
+            _paths.push(paths[i])
+        }
+    }
+
     let queryParams = {}
     if (_url.search && _url.search.length > 0) {
         queryParams = querystring.parse(_url.search.replace('?', ''))
     }
     return {
-        paths,
+        paths: _paths,
         query: queryParams,
     }
 }
@@ -4995,7 +5003,6 @@ exports.send = (res) => {
             _body = JSON.stringify(_body)
             setContentType(res, 'json')
         }
-
         res.write(_body)
         res.end()
         return
